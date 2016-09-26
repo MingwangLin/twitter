619b1571ab55cdd1684bdcf48cdbe93636730be4
MingwangLin <aacheve0@gmail.com>
2016-09-26 11:29:51 +0800

    dsiabled the log function

--- app/api/treelog.py
+++ app/api/treelog.py
@@ -2,9 +2,9 @@ import time
 
 
 def log(*args):
-    t = time.time()
-    tt = time.strftime(r'%Y/%m/%d %H:%M:%S', time.localtime(time.time()))
-    print(tt, *args)
-
+    # t = time.time()
+    # tt = time.strftime(r'%Y/%m/%d %H:%M:%S', time.localtime(time.time()))
+    # print(tt, *args)
+    pass
     # 2016/6/22 21:40:10.000
     # log(1,2,3,4)
