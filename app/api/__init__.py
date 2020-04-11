1fa8270b9a11eebb7813e102fbee74477ad70c98
Mingwang Lin <linmingwang@ata.net.cn>
2020-04-11 13:00:08 +0800

    update blueprint configs

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-api = Blueprint('api', __name__)
+api = Blueprint('api', __name__, template_folder='../static')
 
 from . import login, user, decorator, notification, tweet, repost, comment, follow
